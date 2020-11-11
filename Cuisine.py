61bf4b9653ea68a81bc7baba416e6e16ab39ae69
mvonherrmann-bit <70783680+mvonherrmann-bit@users.noreply.github.com>
2020-11-11 11:02:10 -1000

    Update Cuisine.py

--- Cuisine.py
+++ Cuisine.py
@@ -1,11 +1,133 @@
-from flask import Flask,render_template
+from flask import Flask, flash, render_template, redirect, url_for,request
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy  import SQLAlchemy
+#from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///usertable.db'
+#SQLALCHEMY_BINDS = { 'preferences':  'sqlite:///preferences.db'}
+
+
+
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20),unique=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+
+
+
+
+    
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    name = StringField('Name', validators=[InputRequired(), Length(min=4, max=20)])
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+
+
+
 
 @app.route('/')
 def index():
-	return render_template('index.html')
-	
+    return render_template('index.html')
+
+
+
+@app.route('/Show_Other_Users')
+def Show_Other_Users():
+    return render_template('Show_Other_Users.html')
+
+    
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if (user.password == form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
+
+        return '<h1>Invalid username or password</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('login.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        #hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(name=form.name.data,username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return '<h1>New user has been created!</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('signup.html', form=form)
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', name=current_user.name)
+
+#@app.route('/new_search', methods=['GET', 'POST'])
+#def index():
+    #search = RestaurantSearchForm(request.form)
+    #if request.method == 'POST':
+        #return search_results(search)
+    #return render_template('new_search.html', form=search)
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html',name=current_user.username)
+
+@app.route('/mapviewer')
+@login_required
+def mapviewer():
+    return render_template('mapviewer.html',name=current_user.username)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+#if __name__ == '__main__':
+    #app.run(debug=True)
+
     
 
  
